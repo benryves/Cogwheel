5ad6b4b852c6badebeae6262915904ba83ee13f1
Ben Ryves <benryves@benryves.com>
2007-12-19 22:53:21 +0000

    Addition of .romdata handling code.

---
+++ Sega 8-Bit/Utility/RomData/RomDataFile.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace BeeDevelopment.Sega8Bit.Utility.RomData {
+	/// <summary>
+	/// Represents a .romdata file containing <see cref="RomData"/> instances.
+	/// </summary>
+	public class RomDataFile : IEnumerable<RomData> {
+
+		#region Public Properties
+
+		/// <summary>
+		/// Gets the name of the system.
+		/// </summary>
+		public string System { get; private set; }
+
+		/// <summary>
+		/// Gets the extensions that this data file refers to.
+		/// </summary>
+		public string[] Extensions { get; private set; }
+
+		#endregion
+
+		#region Private Fields
+
+		private List<RomData> Entries;
+
+		#endregion		
+
+		#region Constructors
+		
+		private RomDataFile() {
+			this.Entries = new List<RomData>(); 
+		}
+
+		/// <summary>
+		/// Loads a <see cref="RomDataFile"/> from a file.
+		/// </summary>
+		/// <param name="filename">The name of the file to load the <see cref="RomDataFile"/> from.</param>
+		/// <returns>A <see cref="RomDataFile"/> read from the file <paramref name="filename"/>.</returns>
+		public static RomDataFile FromFile(string filename) {
+			using (var DataFile = new FileStream(filename, FileMode.Open, FileAccess.Read)) {
+				return RomDataFile.FromStream(DataFile);
+			}
+		}
+
+		/// <summary>
+		/// Loads a <see cref="RomDataFile"/> from a <see cref="Stream"/>.
+		/// </summary>
+		/// <param name="stream">The <see cref="Stream"/> to load the <see cref="RomDataFile"/> from.</param>
+		/// <returns>A <see cref="RomDataFile"/> read from the <paramref name="stream"/>.</returns>
+		public static RomDataFile FromStream(Stream stream) {
+			var Result = new RomDataFile();
+
+			var DataFileReader = new StreamReader(stream);
+
+			while (!DataFileReader.EndOfStream) {
+				var Source = DataFileReader.ReadLine();
+				if (Source.Trim() == "") continue;
+				if (Source.StartsWith("!")) {
+					if (Result.System == null) {
+						Result.System = Source.Substring(1).Trim();
+					} else if (Result.Extensions == null) {
+						Result.Extensions = Source.Substring(1).Trim().Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+					}
+				} else {
+					var NewData = RomData.FromString(Source);
+					NewData.DataFile = Result;
+					Result.Entries.Add(NewData);
+				}
+			}
+
+			return Result;
+
+		}
+
+		#endregion
+
+		#region IEnumerable<RomData> Members
+
+		public IEnumerator<RomData> GetEnumerator() {
+			return this.Entries.GetEnumerator();
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+			return this.Entries.GetEnumerator();
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		/// <summary>
+		/// Try and get a <see cref="RomData"/> from the <see cref="RomDataFile"/> by the CRC-32 checksum.
+		/// </summary>
+		/// <param name="crc">The CRC-32 checksum to search for.</param>
+		/// <param name="romData">Outputs the <see cref="RomData"/> instance found matching the supplied <paramref name="crc"/>.</param>
+		/// <returns>True if a matching CRC-32 was found, false otherwise.</returns>
+		public bool TryGetRomData(int crc, out RomData romData) {
+			romData = default(RomData);
+			foreach (var PossibleRom in this) {
+				if (PossibleRom.Crc32 == crc) {
+					romData = PossibleRom;
+					return true;
+				}
+			}
+			return false;
+		}
+
+		#endregion
+	}
+}
---
+++ Sega 8-Bit/Utility/RomData/RomIdentifier.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace BeeDevelopment.Sega8Bit.Utility.RomData {
+
+	/// <summary>
+	/// Provides methods for identification of ROMs.
+	/// </summary>
+	public class RomIdentifier {
+
+		private List<RomDataFile> DataFiles;
+
+		/// <summary>
+		/// Creates an instance of the <see cref="RomIdentifier"/>, loading <see cref="RomDataFile"/> files from a specified directory.
+		/// </summary>
+		/// <param name="path">The path of the directory containing the .romdata files to load.</param>
+		public RomIdentifier(string path) {
+			this.DataFiles = new List<RomDataFile>();
+			foreach (var DataFile in Directory.GetFiles(path, "*.romdata")) {
+				this.DataFiles.Add(RomDataFile.FromFile(DataFile));
+			}
+		}
+
+		/// <summary>
+		/// Try and identify a <see cref="RomData"/> from its CRC-32 checksum.
+		/// </summary>
+		/// <param name="crc">The CRC-32 checksum to search for.</param>
+		/// <param name="romData">Outputs the <see cref="RomData"/> instance found matching the supplied <paramref name="crc"/>.</param>
+		/// <returns>True if a matching CRC-32 was found, false otherwise.</returns>
+		public bool TryGetRomData(int crc, out RomData romData) {
+
+			var PossibleMatches = new List<RomData>();
+
+			foreach (var DataFile in this.DataFiles) {
+				RomData PossibleMatch;
+				if (DataFile.TryGetRomData(crc, out PossibleMatch)) {
+					PossibleMatches.Add(PossibleMatch);
+				}
+			}
+
+			PossibleMatches.Sort();
+
+			if (PossibleMatches.Count == 0) {
+				romData = default(RomData);
+				return false;
+			} else {
+				romData = PossibleMatches[0];
+				return true;
+			}
+
+		}
+
+		/// <summary>
+		/// Try and identify a <see cref="RomData"/> from a binary dump.
+		/// </summary>
+		/// <param name="data">The dump to identify.</param>
+		/// <param name="romData">Outputs the <see cref="RomData"/> instance found matching the supplied <paramref name="data"/>.</param>
+		/// <returns>True if a matching dump was found, false otherwise.</returns>
+		public bool TryGetRomData(byte[] data, out RomData romData) {
+			return this.TryGetRomData(Crc32.Calculate(data), out romData);
+		}
+
+	}
+}
