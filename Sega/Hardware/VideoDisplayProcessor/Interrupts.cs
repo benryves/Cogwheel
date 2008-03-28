e363722de5ca6359981aa49c80de716b4c65a103
Ben Ryves <benryves@benryves.com>
2008-03-28 19:39:12 +0000

    Marked LineInterruptPending and FrameInterruptPending as publicly settable.

--- Sega/Hardware/VideoDisplayProcessor/Interrupts.cs
+++ Sega/Hardware/VideoDisplayProcessor/Interrupts.cs
@@ -4,13 +4,13 @@
 		/// <summary>
 		/// Gets whether a line interrupt is pending or not.
 		/// </summary>
-		public bool LineInterruptPending { get { return this.lineInterruptPending; } }
+		public bool LineInterruptPending { get { return this.lineInterruptPending; } set { this.lineInterruptPending = value; } }
 		private bool lineInterruptPending;
 
 		/// <summary>
 		/// Gets whether a frame interrupt is pending or not.
 		/// </summary>
-		public bool FrameInterruptPending { get { return this.frameInterruptPending; } }
+		public bool FrameInterruptPending { get { return this.frameInterruptPending; } set { this.frameInterruptPending = value; } }
 		private bool frameInterruptPending;
 
 		/// <summary>
